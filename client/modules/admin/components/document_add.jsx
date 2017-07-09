990684f0f640fec15039048b9a8fb3cdcf15cd75
kurapikats <kurapikats@yahoo.com>
2017-07-09 08:46:45 +0800

    add auto doc trackingid rename notes to remarks on doc-add

--- client/modules/admin/components/document_add.jsx
+++ client/modules/admin/components/document_add.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 import {Documents, Logs} from '/lib/collections';
 
 import SchoolDistrictDropdown from '../containers/school_district_dropdown'
@@ -12,16 +13,16 @@ class DocumentAdd extends React.Component {
 
   handleCreate(e) {
     e.preventDefault();
-    const {trackingId, description, notes} = this.refs;
+    const {description, notes} = this.refs;
     let documentId = Meteor.uuid();
 
-    // $('#dtCode').val()
-    // console.log($('#sdCode').val());
-    // console.log($('#dtCode').val());
+    let docTypeId = $('#documentType').val();
+    let dateId = moment().format('MMDDYYHHmmss');
+    let trackingId = `${docTypeId}-${dateId}`;
 
     Documents.insert({
       _id: documentId,
-      trackingId: trackingId.value,
+      trackingId,
       description: description.value,
       notes: notes.value,
       createdBy: Meteor.userId(),
@@ -30,7 +31,7 @@ class DocumentAdd extends React.Component {
 
     Logs.insert({
       documentId,
-      trackingId: trackingId.value,
+      trackingId,
       beginStatus: 'Accepted',
       office: Meteor.user().username,
       dateIn: new Date(),
@@ -38,7 +39,7 @@ class DocumentAdd extends React.Component {
     });
 
     FlowRouter.redirect('/admin/doc-edit/' + documentId);
-    toastr.success(`Document has been saved with TrackingID: ${trackingId.value}`);
+    toastr.success(`Document has been saved with TrackingID: ${trackingId}`);
   }
 
   render() {
@@ -47,25 +48,20 @@ class DocumentAdd extends React.Component {
         <h4>Create Document Tracker</h4>
         <form method="post" class="form-horizontal">
           <div class="form-group">
-            <label class="col-sm-3 control-label">Tracking ID</label>
-            <div class="col-sm-9">
-              <input class="form-control" type="text" name="trackingId" ref="trackingId" defaultValue="" />
-            </div>
-          </div>
-          <div class="form-group">
             <label class="col-sm-3 control-label">Description</label>
             <div class="col-sm-9">
               <textarea class="form-control" name="description" ref="description" defaultValue="" />
             </div>
           </div>
           <div class="form-group">
-            <label class="col-sm-3 control-label">Notes</label>
+            <label class="col-sm-3 control-label">Remarks</label>
             <div class="col-sm-9">
               <textarea class="form-control" name="notes" ref="notes" defaultValue="" />
             </div>
           </div>
 
           <SchoolDistrictDropdown />
+
           <DocumentTypeDropdown />
           <div class="form-group">
             <label class="col-sm-3 control-label">Route</label>
